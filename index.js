2237f8eeba27052f7a17f5a309cabb54f39f6f8a
perry <perry@ionic.io>
2018-08-24 01:26:14 -0500

    prod runs on port 80

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
   res.send(defaultPage);
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
 
 
 function keysToOptions(obj) {
