4d71345e952f0e4cc3f395403b84d694a36c903b
perry <perry@ionic.io>
2018-08-26 17:23:27 -0500

    adding captcha

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 require('dotenv').config();
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const Recaptcha = require('express-recaptcha').Recaptcha;
 const fs = require('fs');
 
+const recaptcha = new Recaptcha(
+  process.env.RECAPTCHA_SITE_KEY, 
+  process.env.RECAPTCHA_SECRET_KEY
+);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
-app.get('/', (req, res) => {
+
+app.get('/', recaptcha.middleware.render, (req, res) => {
   const HTMLBase   = fs.readFileSync('templates/base.html', 'utf8');
   const HTMLForm   = fs.readFileSync('templates/form.html', 'utf8');
   const HTMLStates = fs.readFileSync('templates/states.html', 'utf8');
@@ -14,8 +23,17 @@ app.get('/', (req, res) => {
     .replace('{{content}}', HTMLForm)
     .replace('{{states}}', HTMLStates)
     .replace('{{counties}}', HTMLCounties)
-    .replace('{{issues}}', keysToOptions(issues));
+    .replace('{{issues}}', keysToOptions(issues))
+    .replace('{{captcha}}', res.recaptcha);
   res.send(defaultPage);
+});
+
+app.post('/', recaptcha.middleware.verify, function(req, res){
+  if (!req.recaptcha.error) {
+    console.log(req)
+  } else {
+    console.log(req.recaptcha)
+  }
 });
 
 app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
